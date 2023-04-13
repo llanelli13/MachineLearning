0deaba0057662968a3c2823cbb422fc2d95fc400
ZulieD <80091315+ZulieD@users.noreply.github.com>
2023-04-13 11:02:56 +0200

    suite

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import pandas as pd
 import numpy as np
 
 def prepartation_donnee():
+    #ouvrir fichier csv
     data = pd.read_csv('Data/Data_X.csv.csv')
+    print(data)
+    # Supprime ligne entièrement vide
+    data.dropna(how='all')
+    print(data)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
