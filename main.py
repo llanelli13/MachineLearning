cf1ca183f14412104b32af3c64108d41cc6a6823
ZulieD <80091315+ZulieD@users.noreply.github.com>
2023-04-13 10:54:42 +0200

    début

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-if __name__ == "__main__":
-    print("Coucou")
+import pandas as pd
+import numpy as np
+
+def prepartation_donnee():
+    data = pd.read_csv('Data/Data_X.csv.csv')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    prepartation_donnee()
