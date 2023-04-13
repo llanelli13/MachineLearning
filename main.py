d30b19479e88afc796a899148f3344ee74449d74
ZulieD <80091315+ZulieD@users.noreply.github.com>
2023-04-13 11:09:13 +0200

    suite

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import numpy as np
 
 def prepartation_donnee():
     #ouvrir fichier csv
-    data = pd.read_csv('Data/Data_X.csv.csv')
+    data = pd.read_csv('Data/Data_X.csv')
     print(data)
     # Supprime ligne entièrement vide
-    data.dropna(how='all')
+    data_notnull=data.dropna(how='all')
     print(data)
+
+    # met O sur case null
+    data_dim_fill = data_notnull.fillna(0)
+    print(data_dim_fill)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
