e344d4d7425f8b243c7e9bb12e1045ce5426063a
ZulieD <80091315+ZulieD@users.noreply.github.com>
2023-04-18 09:50:15 +0200

    preparation1

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
+#import seaborn as sns
 
 
 def prepartation_donnee():
+
     #ouvrir fichier csv
-    data = pd.read_csv('Data/Data_X.csv')
-    print(data)
+    data_x = pd.read_csv('Data/Data_X.csv')
+    data_y = pd.read_csv("Data/Data_Y.csv")
+
+    # Fusionner les données d'entrée et de sortie en fonction de l'ID
+    merged_data = pd.merge(data_x, data_y, on='ID')
+
+    print(merged_data)
 
     # Supprime ligne entièrement vide
-    data_notnull=data.dropna(how='all')
-    print(data)
+    data_notnull=merged_data.dropna(how='all')
+    print(data_notnull)
 
     # met O sur case null
     data_dim_fill = data_notnull.fillna(0)
     print(data_dim_fill)
 
 
+"""
 def jspcestquoilafonctionmaistqt():
     # Charger les données d'entrée Data X et de sortie Data Y
     data_x = pd.read_csv("Data/Data_X.csv")
-    data_y = pd.read_csv("Data/Data_Y.csv")
+
 
     # Fusionner les données d'entrée et de sortie en fonction de l'ID
-    merged_data = pd.merge(data_x, data_y, on='ID')
+
 
     # Vérifier s'il y a des valeurs manquantes
     print(merged_data.isna().sum())
@@ -34,7 +41,7 @@ def jspcestquoilafonctionmaistqt():
     # Normalisation des données
     normalized_data = (merged_data - merged_data.mean()) / merged_data.std()
     print(normalized_data.head())
-
+"""
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
